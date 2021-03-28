2b9817193c44d324a4a8ff8f588c6ffe85d07b62
ThomasAndon <513230863@qq.com>
2021-03-28 19:37:32 +0800

    删除hello

--- src/GUI/hello.java
+++
@@ -1,7 +0,0 @@
-package GUI;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println(1);
-    }
-}
---
+++ src/NetBeans/UserBean.java
@@ -0,0 +1,24 @@
+package NetBeans;
+
+
+
+/**
+* @Description: This is the bean class of users
+* @Author: Thomas Andon
+* @Date: 2021/3/28
+*/
+public class UserBean {
+    private String name;
+    private String id;
+    private String password;
+    private String gender;
+    private String motto;
+    private double height;
+    private double weight;
+    private int level;
+
+
+
+
+
+}
