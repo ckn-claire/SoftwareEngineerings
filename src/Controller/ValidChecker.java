a41728bbeaf9f8d501ec64ef27d56e9bd22678a5
ThomasAndon <513230863@qq.com>
2021-03-29 21:22:02 +0800

    UI，loginpage优化，逻辑说明。添加合法检查器的class

---
+++ src/Controller/ValidChecker.java
@@ -0,0 +1,11 @@
+package Controller;
+
+
+/**
+* @Description:  This class contains all methods that checks the validation.
+* @Date: 2021/3/29
+*/
+public class ValidChecker {
+    //todo 所有需要检查的方法都写在这里，比如检查用户名、密码是否符合规范，等等。
+
+}
