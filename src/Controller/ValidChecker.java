0ebf05e61ff80b764e7efc267e2664a39eea6565
ThomasAndon <513230863@qq.com>
2021-03-29 21:32:01 +0800

    合法检查器的class

--- src/Controller/ValidChecker.java
+++ src/Controller/ValidChecker.java
@@ -2,10 +2,37 @@ package Controller;
 
 
 /**
-* @Description:  This class contains all methods that checks the validation.
-* @Date: 2021/3/29
-*/
+ * @Description: This class contains all methods that checks the validation.
+ * @Date: 2021/3/29
+ */
 public class ValidChecker {
     //todo 所有需要检查的方法都写在这里，比如检查用户名、密码是否符合规范，等等。
+
+
+    public boolean isIDValid(String id) {
+        //todo 实现id检查逻辑，只能包括大小写字母和数字，长度在6-15位。
+        return false;
+    }
+
+    public boolean isPwValid(String pw) {
+
+        //Password follows the same rules with ID.
+        return isIDValid(pw);
+    }
+
+    
+    /** 
+    * @Description:  
+    * @Param:  
+    * @return:  
+    * @Author: Thomas Andon
+    * @Date: 2021/3/29
+    */
+    public boolean isSameString (String s1, String s2) {
+        return s1.equals(s2);
+    }
+
+    
+
 
 }
